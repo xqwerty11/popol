dbcc6bd2913f3e4850c879dfb7ca895ba6e07822
xqwerty11 <hk637999@gmail.com>
2023-10-05 10:22:47 +0900

    유튜브 데이터 출력

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,11 +1,42 @@
 import Layout from '../../common/layout/Layout';
-//import styles from './Department.module.scss';
-//import clsx from 'clsx';
+import './Youtube.scss';
+import { useEffect, useState } from 'react';
 
 export default function Youtube() {
+	const [Youtube, setYoutube] = useState([]);
+
+	const fetchYoutube = () => {
+		const api_key = 'AIzaSyDPnHs8VjFYmGy7yyptLrtUMuO_AaTnnog';
+		const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+		const pid = 'PLxnkDxSlsKAFL-bto9b2pduWdqoYQazhW';
+		const num = 10;
+		const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+
+		fetch(resultURL)
+			.then((data) => data.json())
+			.then((json) => {
+				console.log(json);
+				setYoutube(json.items);
+			});
+	};
+
+	useEffect(() => {
+		fetchYoutube();
+	}, []);
+
 	return (
 		<Layout title={'Youtube'}>
-			<p>유튜브 페이지입니다.</p>
+			{Youtube.map((data, idx) => {
+				return (
+					<article key={idx}>
+						<h2>{data.snippet.title}</h2>
+						<p>{data.snippet.description}</p>
+						<div className='pic'>
+							<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
