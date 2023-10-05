8577811aef12932095e20a089b5dd1e1bfc15444
xqwerty11 <hk637999@gmail.com>
2023-10-05 15:30:39 +0900

    마커 위치 보정하기

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -8,14 +8,14 @@ export default function Contact() {
 	//리액트 컴포넌트안쪽에서 window객체로부터 kakao객체를 비구조화할당을 이용해서 수동으로 꺼내옴
 	const { kakao } = window;
 	const mapOption = {
-		center: new kakao.maps.LatLng(37.584725, 126.885511), // 지도의 중심좌표
+		center: new kakao.maps.LatLng(37.58478611732263, 126.88563592971315), // 지도의 중심좌표
 		level: 1, // 지도의 확대 레벨
 	};
 	//마커위치가 출력될 인스턴스 객체 생성
-	const markerPosition = new kakao.maps.LatLng(37.584725, 126.885511);
+
 	//마커 위치 인스턴스를 인수로 전달해서 마커 출력 인스턴스 객체를 생성
 	const marker = new kakao.maps.Marker({
-		position: markerPosition,
+		position: mapOption.center,
 	});
 
 	useEffect(() => {
