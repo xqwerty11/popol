db080791b0299c234de439d4c4cadc0f880845b8
xqwerty11 <hk637999@gmail.com>
2023-10-05 13:31:54 +0900

    forwardRef, useImperativeHandle로 자식 컴포넌트의 state변경함수를 역으로 부모에게 전달

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,19 +1,23 @@
 import Layout from '../../common/layout/Layout';
 import Modal from '../../common/modal/Modal';
 import './Youtube.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 /*
 	리액트는 단방향 데이터 바인딩
 	- 부모에서 자식으로 데이터 전달가능하지만 자식에서 부모로는 데이터를 전달 불가
 	- props전달, children 전달
 	
-	- 리액트에서 자식 컴포넌트에서는 직접적으로 부모 컴포넌트의 state값 변경이 불가
-		- 해결방법 부모의 state변경함수를 자식 컴포넌트로 전달
-		- 자식컴포넌트에서는 전달받은 state변경함수로 간접적으로 부모 state값 변경가능
+	리액트에서 자식 컴포넌트에서는 직접적으로 부모 컴포넌트의 state값 변경이 불가
+	- 해결방법 부모의 state변경함수를 자식 컴포넌트로 전달
+	- 자식컴포넌트에서는 전달받은 state변경함수로 간접적으로 부모 state값 변경가능
+
+	useRef로 jsx는 참조 객체에 담을 수 있음
+	컴포넌트를 useRef를 통한 참조객체 담는것이 불가
 */
 
 export default function Youtube() {
+	const refEl = useRef(null);
 	const [Youtube, setYoutube] = useState([]);
 	const [IsModal, setIsModal] = useState(false);
 
@@ -42,9 +46,11 @@ export default function Youtube() {
 				{Youtube.map((data, idx) => {
 					return (
 						<article key={idx}>
-							<h2>{data.snippet.title}</h2>
+							<h2 onClick={() => console.log(refEl)}>{data.snippet.title}</h2>
+
 							<p>{data.snippet.description}</p>
-							<div className='pic' onClick={() => setIsModal(true)}>
+
+							<div className='pic' onClick={() => refEl.current.open()}>
 								<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
 							</div>
 						</article>
@@ -52,7 +58,7 @@ export default function Youtube() {
 				})}
 			</Layout>
 
-			{IsModal && <Modal setIsModal={setIsModal}></Modal>}
+			<Modal setIsModal={setIsModal} ref={refEl}></Modal>
 		</>
 	);
 }
