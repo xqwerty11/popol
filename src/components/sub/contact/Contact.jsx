589d321776c128a5de03886039117eebd0c2fd62
xqwerty11 <hk637999@gmail.com>
2023-10-05 15:07:13 +0900

    카카오 지도 불러오기

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,10 +1,22 @@
 import Layout from '../../common/layout/Layout';
-import styles from './Contact.scss';
+import './Contact.scss';
+import { useRef, useEffect } from 'react';
 
 export default function Contact() {
+	const map = useRef(null);
+	//현재 kakao 객체를 cdn으로 가져오고 있기 떄문에
+	//리액트 컴포넌트안쪽에서 window객체로부터 kakao객체를 비구조화할당을 이용해서 수동으로 꺼내옴
+	const { kakao } = window;
+	const mapOption = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		level: 3, // 지도의 확대 레벨
+	};
+	useEffect(() => {
+		new kakao.maps.Map(map.current, mapOption);
+	}, []);
 	return (
 		<Layout title={'Contact'}>
-			<p>컨택트 페이지입니다.</p>
+			<div className='map' ref={map}></div>
 		</Layout>
 	);
 }
