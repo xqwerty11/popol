a25cf9fdf7aa46b4642d1777d71e3343654738da
xqwerty11 <hk637999@gmail.com>
2023-10-05 11:21:20 +0900

    유튜브 컴포넌트에서 모달컴포넌트 호출

---
+++ src/components/common/modal/Modal.jsx
@@ -0,0 +1,12 @@
+import './Modal.scss';
+
+function Modal() {
+	return (
+		<aside className='modal'>
+			<div className='con'></div>
+			<span>close</span>
+		</aside>
+	);
+}
+
+export default Modal;
