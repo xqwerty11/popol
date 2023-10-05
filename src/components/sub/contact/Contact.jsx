d6cb8dd0b3d03b872f8e7067a74051e9732cbd68
xqwerty11 <hk637999@gmail.com>
2023-10-05 16:22:12 +0900

    교통정보 보기 이벤트 연결

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -4,10 +4,9 @@ import { useRef, useEffect } from 'react';
 
 export default function Contact() {
 	const map = useRef(null);
-	//현재 kakao객체를 cdn으로 가져오고 있기 때문에
-	//리액트 컴포넌트안쪽에서 window객체로부터 kakao객체를 비구조화할당을 이용해서 수동으로 꺼내옴
-	const { kakao } = window;
+	const instance = useRef(null);
 
+	const { kakao } = window;
 	//첫번째 지도를 출력하기 위한 객체정보
 	const info = {
 		latlng: new kakao.maps.LatLng(37.58478163978524, 126.88566424098676),
@@ -15,7 +14,6 @@ export default function Contact() {
 		imgSize: new kakao.maps.Size(232, 99),
 		imgPos: { offset: new kakao.maps.Point(116, 99) },
 	};
-
 	//위의 정보값을 활용한 마커 객체 생성
 	const marker = new kakao.maps.Marker({
 		position: info.latlng,
@@ -24,15 +22,22 @@ export default function Contact() {
 
 	useEffect(() => {
 		//객체 정보를 활용한 지도 객체 생성
-		const instance = new kakao.maps.Map(map.current, {
+		instance.current = new kakao.maps.Map(map.current, {
 			center: info.latlng,
 			level: 1,
 		});
 		//마커 객체에 지도 객체 연결
-		marker.setMap(instance);
+		marker.setMap(instance.current);
 	}, []);
+
 	return (
 		<Layout title={'Contact'}>
+			<button onClick={() => instance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)}>
+				주변 교통정보 보기
+			</button>
+			<button onClick={() => instance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)}>
+				주변 교통정보 끄기
+			</button>
 			<div className='map' ref={map}></div>
 		</Layout>
 	);
