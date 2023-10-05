cb0018d4c37a13dde01e03ec4dfb9589a0ef0a89
xqwerty11 <hk637999@gmail.com>
2023-10-05 15:43:30 +0900

    마커 이미지 변경

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -7,15 +7,21 @@ export default function Contact() {
 	//현재 kakao 객체를 cdn으로 가져오고 있기 떄문에
 	//리액트 컴포넌트안쪽에서 window객체로부터 kakao객체를 비구조화할당을 이용해서 수동으로 꺼내옴
 	const { kakao } = window;
+
 	const mapOption = {
 		center: new kakao.maps.LatLng(37.58478611732263, 126.88563592971315), // 지도의 중심좌표
 		level: 1, // 지도의 확대 레벨
 	};
-	//마커위치가 출력될 인스턴스 객체 생성
+
+	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
+	const imageSize = new kakao.maps.Size(232, 99);
+	const imageOption = { offset: new kakao.maps.Point(116, 99) };
+	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
 	//마커 위치 인스턴스를 인수로 전달해서 마커 출력 인스턴스 객체를 생성
 	const marker = new kakao.maps.Marker({
 		position: mapOption.center,
+		image: markerImage,
 	});
 
 	useEffect(() => {
