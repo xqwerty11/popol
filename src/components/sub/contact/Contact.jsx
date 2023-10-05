e6749f61627a79b0499f8e25fb7426353d620e93
xqwerty11 <hk637999@gmail.com>
2023-10-05 16:14:33 +0900

    코드 정리 및 설명 추가

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -8,6 +8,7 @@ export default function Contact() {
 	//리액트 컴포넌트안쪽에서 window객체로부터 kakao객체를 비구조화할당을 이용해서 수동으로 꺼내옴
 	const { kakao } = window;
 
+	//첫번째 지도를 출력하기 위한 객체정보
 	const info = {
 		latlng: new kakao.maps.LatLng(37.58478163978524, 126.88566424098676),
 		imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
@@ -15,19 +16,19 @@ export default function Contact() {
 		imgPos: { offset: new kakao.maps.Point(116, 99) },
 	};
 
-	//마커 위치 인스턴스를 인수로 전달해서 마커 출력 인스턴스 객체를 생성
+	//위의 정보값을 활용한 마커 객체 생성
 	const marker = new kakao.maps.Marker({
 		position: info.latlng,
 		image: new kakao.maps.MarkerImage(info.imgSrc, info.imgSize, info.imgPos),
 	});
 
 	useEffect(() => {
-		//컴포넌트 마운트 되자마자 지도인스턴스 생성
+		//객체 정보를 활용한 지도 객체 생성
 		const instance = new kakao.maps.Map(map.current, {
 			center: info.latlng,
 			level: 1,
 		});
-		//마커 출력 인스턴스에 지도 인스턴스 결합
+		//마커 객체에 지도 객체 연결
 		marker.setMap(instance);
 	}, []);
 	return (
