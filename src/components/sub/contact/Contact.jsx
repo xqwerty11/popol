1980ad90aab755c38e33c9969fd2666d81937f63
xqwerty11 <hk637999@gmail.com>
2023-10-05 09:27:36 +0900

    모듈 사스 > 기본 사스로 변경

---
+++ src/components/sub/contact/Contact.jsx
@@ -0,0 +1,10 @@
+import Layout from '../../common/layout/Layout';
+import styles from './Contact.scss';
+
+export default function Contact() {
+	return (
+		<Layout title={'Contact'}>
+			<p>컨택트 페이지입니다.</p>
+		</Layout>
+	);
+}
