1ffcf737eefde882f0c1f5cdbdf2fb9f3e8b00d1
xqwerty11 <hk637999@gmail.com>
2023-10-06 16:24:23 +0900

    갤러리 데이터 받아오기

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,7 +1,20 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
+import { useEffect, useState } from 'react';
 
 export default function Gallery() {
+	const api_key = 'bbf48601ef45cb60f5bcfdb652b8bfa4';
+	const method_interest = 'flickr.interestingness.getList';
+	const num = 500;
+	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+
+	useEffect(() => {
+		fetch(url)
+			.then((data) => data.json())
+			.then((json) => {
+				console.log(json);
+			});
+	}, []);
 	return (
 		<Layout title={'Gallery'}>
 			<p>갤러리 페이지입니다.</p>
