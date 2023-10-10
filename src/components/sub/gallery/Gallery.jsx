b03632bf8cf416a254b07e664a666121cfa236ba
xqwerty11 <hk637999@gmail.com>
2023-10-10 13:13:18 +0900

    이미지 에러이벤트 발생시 대체 이미지 출력

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -3,8 +3,6 @@ import './Gallery.scss';
 import { useEffect, useState, useRef } from 'react';
 import Masonry from 'react-masonry-component';
 
-//메서니 옵션 추가
-
 const my_id = '199282981@N03';
 
 export default function Gallery() {
@@ -15,11 +13,8 @@ export default function Gallery() {
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
 		const method_search = 'flickr.photos.search';
-
 		const num = 50;
 
-		//fetching함수 호출시 타입값이 있는 객체를 인수로 전달하면 해당 타입에 따라 호출 URL이 변경되고
-		//해당URL을 통해 받아지는 데이터로 달라짐
 		if (opt.type === 'interest') {
 			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
 		}
@@ -30,30 +25,25 @@ export default function Gallery() {
 			url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&tags=${opt.tags}`;
 		}
 
-		//만약 특정함수가 promise를 반환한다면 warpping함수로 묶어준뒤 async 지정
-		//각각의 promise 반환 함수 앞쪽에 await을 붙이기만 하면 해당 코드는 동기화됨
-		//지저분하게 depth를 들여쓰기 해가면서 then구문을 호출할 필요가 없음
 		const data = await fetch(url);
 		const json = await data.json();
+		if (json.photos.photo.length === 0) {
+			return alert('해당 검색어의 결과값이 없습니다');
+		}
 		setPics(json.photos.photo);
 	};
 	const refInput = useRef(null);
 
 	useEffect(() => {
-		//type: 'interest' 인터레스트 방식 갤러리 호출
-		//type: 'user' 사용자 아이디 계정의 갤러리 호출
 		fetchData({ type: 'user', id: my_id });
-		//fetchData({ type: 'interest' });
-		//fetchData({ type: 'search', tags: 'ocean' });
 	}, []);
 
 	return (
 		<Layout title={'Gallery'}>
 			<form
 				onSubmit={(e) => {
-					//submit이벤트의 기본 서버 전송기능을 막아줌
 					e.preventDefault();
-					//문자열.trim() : 문자열앞뒤로 빈칸을 제거해서 정리
+
 					if (refInput.current.value.trim() === '') {
 						return alert('검색어를 입력하세요.');
 					}
@@ -91,6 +81,11 @@ export default function Gallery() {
 										<img
 											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
 											alt=''
+											//만약 사용자가 프로필 이미지를 올리지 않았을때 엑박이 뜨므로
+											//onError이벤트를 연결해서 대체이미지 출력
+											onError={(e) => {
+												e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
+											}}
 										/>
 										<span onClick={() => fetchData({ type: 'user', id: data.owner })}>
 											{data.owner}
