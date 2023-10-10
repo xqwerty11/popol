0d87c6510665448514200a6f1a06c15d18ca3342
xqwerty11 <hk637999@gmail.com>
2023-10-10 11:11:45 +0900

    사용자 아이디 클릭시 이미지 출력

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -73,7 +73,9 @@ export default function Gallery() {
 											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
 											alt=''
 										/>
-										<span>{data.owner}</span>
+										<span onClick={() => fetchData({ type: 'user', id: data.owner })}>
+											{data.owner}
+										</span>
 									</div>
 								</div>
 							</article>
