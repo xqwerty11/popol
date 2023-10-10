59a3403e3e7d59f81f18bf1067d717a459a31dce
xqwerty11 <hk637999@gmail.com>
2023-10-10 11:25:47 +0900

    검색 메서드 추가

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -17,6 +17,7 @@ export default function Gallery() {
 		const api_key = 'bbf48601ef45cb60f5bcfdb652b8bfa4';
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flickr.photos.search';
 
 		const num = 50;
 
@@ -27,6 +28,9 @@ export default function Gallery() {
 		}
 		if (opt.type === 'user') {
 			url = `https://www.flickr.com/services/rest/?method=${method_user}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&user_id=${opt.id}`;
+		}
+		if (opt.type === 'search') {
+			url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&tags=${opt.tags}`;
 		}
 
 		//만약 특정함수가 promise를 반환한다면 warpping함수로 묶어준뒤 async 지정
@@ -40,8 +44,9 @@ export default function Gallery() {
 	useEffect(() => {
 		//type: 'interest' 인터레스트 방식 갤러리 호출
 		//type: 'user' 사용자 아이디 계정의 갤러리 호출
-		fetchData({ type: 'user', id: my_id });
+		//fetchData({ type: 'user', id: my_id });
 		//fetchData({ type: 'interest' });
+		fetchData({ type: 'search', tags: 'ocean' });
 	}, []);
 
 	return (
