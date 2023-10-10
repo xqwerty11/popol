608c30e58d90e5c88cca3911bf3067ae1e9cad2a
xqwerty11 <hk637999@gmail.com>
2023-10-10 09:48:58 +0900

    masonry ui 적용

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,6 +1,12 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 import { useEffect, useState } from 'react';
+import Masonry from 'react-masonry-component';
+
+//메서니 옵션 추가
+const masonryOptions = {
+	transitionDuration: 0,
+};
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
@@ -20,29 +26,37 @@ export default function Gallery() {
 	return (
 		<Layout title={'Gallery'}>
 			<div className='picFrame'>
-				{Pics.map((data, idx) => {
-					return (
-						<article key={idx}>
-							<div className='inner'>
-								<img
-									className='pic'
-									src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
-									alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
-								/>
-
-								<h2>{data.title}</h2>
-
-								<div className='profile'>
+				{/* 반복 돈ㄴ article요소를 Masonry로 wrapping후 세팅 */}
+				<Masonry
+					elementType={'div'} //Manonry컴포넌트가 변환될 태그명 지정
+					options={{ transitionDuration: '0.5s' }} //박스모션시 transition 시간 설정
+					disableImagesLoaded={false} //true 이미지 로딩처리 안함
+					updateOnEachImageLoad={false} //true 각 이미지 로딩처리 안함
+				>
+					{Pics.map((data, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
 									<img
-										src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
-										alt=''
+										className='pic'
+										src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
+										alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
 									/>
-									<span>{data.owner}</span>
+
+									<h2>{data.title}</h2>
+
+									<div className='profile'>
+										<img
+											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
+											alt=''
+										/>
+										<span>{data.owner}</span>
+									</div>
 								</div>
-							</div>
-						</article>
-					);
-				})}
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
