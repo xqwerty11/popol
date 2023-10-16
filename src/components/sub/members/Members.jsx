48f13b5a2985c40a56b1bf3765c909864859136a
xqwerty11 <hk637999@gmail.com>
2023-10-16 10:46:47 +0900

    기본 인증 로직 완성

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -20,13 +20,20 @@ export default function Members() {
 
 	//인수값으로 state를 전달받아서 각 데이터별로 인증처리후
 	//만약 인증에러가 발생하면 해당 name값으로 에러문구를 생성해서 반환하는 함수
-	const check = (value) => {};
+	const check = (value) => {
+		const errs = {};
+		if (value.userid.length < 5) {
+			errs.userid = '아이디는 최소 5글자 이상 입력하세요';
+		}
+		return errs;
+	};
 
 	//전송이벤트 발생시 state에 있는 인풋값들을 check함수에 전달해서 호출
 	//만약 check함수가 에러객체를 하나도 내보내지 않으면 인증성공
 	//하나라도 에러객체가 전달되면 인증실패처리하면서 name값과 매칭이 되는 input요소 아래쪽에 에러메세지 출력
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		console.log(check(Val));
 	};
 	return (
 		<Layout title={'Members'}>
