4615e9cd3e1f8c6d8a5c58c5b82ddabddf2ed7ed
xqwerty11 <hk637999@gmail.com>
2023-10-16 09:37:56 +0900

    기본 인풋요소 입력

--- src/components/main/news/News.jsx
+++ src/components/main/news/News.jsx
@@ -2,6 +2,9 @@ import './News.scss';
 import { useState } from 'react';
 
 function News() {
+	//프로젝트가 처음 구동되면 무조건 메인페이지에 갔다가 커뮤니티 페이지에 넘어가는 구조
+	//해당 페이지에 있는 함수가 처음 구동될시 로컬저장소에 값이 없으므로
+	//저장소값이 없을때 빈배열이 반환되는 구문을 추가 (주요)
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
 		if (data) return JSON.parse(data);
--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -4,7 +4,42 @@ import './Members.scss';
 export default function Members() {
 	return (
 		<Layout title={'Members'}>
-			<p>회원가입 페이지입니다.</p>
+			<form>
+				<fieldset>
+					<legend>회원가입 폼 양식</legend>
+					<table border='1'>
+						<tbody>
+							{/* userid */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='userid'>userid</label>
+								</th>
+								<td>
+									<input type='text' id='userid' name='userid' />
+								</td>
+							</tr>
+							{/* password */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd1'>password</label>
+								</th>
+								<td>
+									<input type='password' id='pwd1' name='pwd1' />
+								</td>
+							</tr>
+							{/* re password */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd2'>re-password</label>
+								</th>
+								<td>
+									<input type='password' id='pwd2' name='pwd2' />
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
 		</Layout>
 	);
 }
