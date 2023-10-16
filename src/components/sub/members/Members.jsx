d9502287c1e47faf0b7ba213ef672e7c47284745
xqwerty11 <hk637999@gmail.com>
2023-10-16 10:13:35 +0900

    handleChange핸들러 모든 인풋에 연결

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -1,7 +1,20 @@
 import Layout from '../../common/layout/Layout';
 import './Members.scss';
+import { useState } from 'react';
 
 export default function Members() {
+	const initVal = {
+		userid: '',
+		pwd1: '',
+		pwd2: '',
+		email: '',
+	};
+
+	const [Val, setVal] = useState(initVal);
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		console.log(name, value);
+	};
 	return (
 		<Layout title={'Members'}>
 			<form>
@@ -15,7 +28,13 @@ export default function Members() {
 									<label htmlFor='userid'>userid</label>
 								</th>
 								<td>
-									<input type='text' id='userid' name='userid' />
+									<input
+										type='text'
+										id='userid'
+										name='userid'
+										value={Val.userid}
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 							{/* password */}
@@ -24,7 +43,13 @@ export default function Members() {
 									<label htmlFor='pwd1'>password</label>
 								</th>
 								<td>
-									<input type='password' id='pwd1' name='pwd1' />
+									<input
+										type='password'
+										id='pwd1'
+										name='pwd1'
+										value={Val.pwd1}
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 							{/* re password */}
@@ -33,7 +58,28 @@ export default function Members() {
 									<label htmlFor='pwd2'>re-password</label>
 								</th>
 								<td>
-									<input type='password' id='pwd2' name='pwd2' />
+									<input
+										type='password'
+										id='pwd2'
+										name='pwd2'
+										value={Val.pwd2}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+
+							<tr>
+								<th scope='row'>
+									<label htmlFor='email'>email</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='email'
+										name='email'
+										value={Val.email}
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 
