bb53b1680451a7eb13437be229b691ecc4df0225
xqwerty11 <hk637999@gmail.com>
2023-10-18 14:19:44 +0900

    department 페이지 설명

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,3 +1,5 @@
+//1.해당 페이지를 어떤식으로 작업했고 어떤 이슈가 있었는지 설명
+
 import Layout from '../../common/layout/Layout';
 import { useEffect, useState } from 'react';
 import './Department.scss';
@@ -94,3 +96,5 @@ export default function Department() {
 		</Layout>
 	);
 }
+
+//답변 - 정적인 데이터라서 굳이 fecth를 통해서 데이터를 가져오지 않고 static하게 컨텐츠를 집어넣을까 고민도 했지만 데이터기반으로 모든 화면단이 동적으로 생성되게 하고 싶어서 fetch를 통해서 데이터가 변경되더라도 자동으로 화면이 갱신되도록 작업을 했다.
