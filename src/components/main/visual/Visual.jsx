f49ba91f88fb022f2ea2208f33d57d2d730ddf8a
xqwerty11 <hk637999@gmail.com>
2023-10-19 11:19:19 +0900

    기본 swiper 패널 스타일링

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -7,13 +7,17 @@ function Visual() {
 	const { data } = useSelector((store) => store.youtube);
 	return (
 		<section className='visual'>
-			<Swiper>
+			<Swiper slidesPerView={3} spaceBetween={50} centeredSlides={true} loop={true}>
 				{data.map((vid, idx) => {
 					if (idx >= 5) return null;
 					return (
 						<SwiperSlide key={idx}>
 							<div className='inner'>
+								<div className='pic'>
+									<img src={vid.snippet.thumbnails.maxres.url} alt={vid.title} />
+								</div>
 								<h2>{vid.snippet.title}</h2>
+								<button>VIEW</button>
 							</div>
 						</SwiperSlide>
 					);
