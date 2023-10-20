ced6b34e9a4369aea7439ce926c66b749c3ab75d
xqwerty11 <hk637999@gmail.com>
2023-10-20 15:43:34 +0900

    react-query의 데이터 상태

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,13 +1,11 @@
 //해당 유튜브 페이지 작업에 대해서 설명, 이슈사항은 없었는지
-
 import Layout from '../../common/layout/Layout';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
 import './Youtube.scss';
+import { useYoutubeQuery } from '../../../hooks/useYoutube';
 
 export default function Youtube() {
-	const Youtube = useSelector((store) => store.youtube.data);
+	const { data, isSuccess } = useYoutubeQuery();
 	return (
 		<>
 			<Layout title={'PLAYLIST'}>
@@ -19,31 +17,32 @@ export default function Youtube() {
 					</p>
 				</div>
 
-				{Youtube.map((data, idx) => {
-					let tit = data.snippet.title;
-					let desc = data.snippet.description;
-					let date = data.snippet.publishedAt;
-					return (
-						<article key={idx}>
-							<div className='Youtubebox'>
-								{/* 제목 */}
-								<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
-								{/* 내용 */}
-								<p>{desc.length > 120 ? desc.substr(0, 120) + '...' : desc}</p>
-								{/* 날짜 */}
-								<span>{date.split('T')[0].split('-').join('.')}</span>
-							</div>
+				{isSuccess &&
+					data.map((data, idx) => {
+						let tit = data.snippet.title;
+						let desc = data.snippet.description;
+						let date = data.snippet.publishedAt;
+						return (
+							<article key={idx}>
+								<div className='Youtubebox'>
+									{/* 제목 */}
+									<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
+									{/* 내용 */}
+									<p>{desc.length > 120 ? desc.substr(0, 120) + '...' : desc}</p>
+									{/* 날짜 */}
+									<span>{date.split('T')[0].split('-').join('.')}</span>
+								</div>
 
-							{/* 썸네일 */}
-							<div className='pic'>
-								{/* 썸네일 링크 클릭시 특정유튜브 객체 하나의 정보값을 받기 위해서 유튜브 객체의 id값을 params로 전달 */}
-								<Link to={`/detail/${data.id}`}>
-									<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
-								</Link>
-							</div>
-						</article>
-					);
-				})}
+								{/* 썸네일 */}
+								<div className='pic'>
+									{/* 썸네일 링크 클릭시 특정유튜브 객체 하나의 정보값을 받기 위해서 유튜브 객체의 id값을 params로 전달 */}
+									<Link to={`/detail/${data.id}`}>
+										<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
+									</Link>
+								</div>
+							</article>
+						);
+					})}
 			</Layout>
 		</>
 	);
--- src/hooks/useYoutube.js
+++ src/hooks/useYoutube.js
@@ -15,6 +15,18 @@ const fetchYoutube = async () => {
 //react-query에는 쿼리키를 문자열로 지정해서 데이터 호출시 쿼리키가 동일하면 동일한 데이터로 인지해서
 //refetching처리하지 않고 캐싱되어 있는 데이터를 재활용
 //useQuery([쿼리키], fecthing함수,캐싱옵션)
+
+//react-query에서 쓰이는 데이터 상태 개념
+
+//pending : 데이터 요청을 보내고 응답을 받기까지의 상태
+//fresh : 작업자가 직접 지정한 데이터의 신선한 상태
+//stale : 작업자가 직접 지정한 데이터의 신선하지 못한 옛날 데이터의 상태
+//inactive : 해당 컴포넌트에서 리액트쿼리로 관리하고 있는 데이터를 활용하고 있지 않은 비활성화 상태
 export const useYoutubeQuery = () => {
-	return useQuery(['youtubeData'], fetchYoutube);
+	return useQuery(['youtubeData'], fetchYoutube, {
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+		cacheTime: 1000 * 10,
+		staleTime: 1000 * 10,
+	});
 };
