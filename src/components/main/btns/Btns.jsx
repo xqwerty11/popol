f9cf22551e750af09299ed81219c8653a20b504d
xqwerty11 <hk637999@gmail.com>
2023-10-20 13:16:17 +0900

    브라우저 리사이즈시 현재 활성화되어 있는 순번의 섹션 위치값으로 스크롤 위치값 보정처리

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -18,13 +18,23 @@ function Btns() {
 		setNum(pos.current.length);
 	};
 
+	//브라우저 리사이즈시 현재 세로 스크롤값을 갱신하는 함수
+	const modifyPos = () => {
+		let activeIndex = 0;
+		const lis = refBtns.current.querySelectorAll('li');
+		lis.forEach((li, idx) => {
+			li.classList.contains('on') && (activeIndex = idx);
+		});
+		window.scrollTo(0, pos.current[activeIndex]);
+	};
+
 	//브라우저 스크롤시 버튼을 반복돌면서 스크롤리 특정 섹션영역을 넘어가면 해당 순번에 버튼 활성화 함수
 	const activation = () => {
 		const btns = refBtns.current.querySelectorAll('li');
 		const scroll = window.scrollY;
 
 		pos.current.forEach((el, idx) => {
-			if (scroll >= el) {
+			if (scroll >= el - window.innerHeight / 2.5) {
 				for (let btn of btns) btn.classList.remove('on');
 				btns[idx].classList.add('on');
 			}
@@ -36,7 +46,9 @@ function Btns() {
 
 	useEffect(() => {
 		getPos();
+
 		window.addEventListener('resize', throttledGetPos);
+		window.addEventListener('resize', modifyPos());
 		window.addEventListener('scroll', throttleActivation);
 
 		return () => {
